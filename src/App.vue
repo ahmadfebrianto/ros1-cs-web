10561432c83d2fb9681df65b6b6d4d63cd3cbf22
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-03-14 10:22:02 +0700

    Resize the card view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="card" style="margin-bottom : 100px">
+  <div class="card" style="margin-bottom: 100px">
     <div class="card-header">
       <h3>ROS UI</h3>
     </div>
 
     <div class="card-body">
       <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-4">
           <div class="card">
             <div class="card-header">
               <div class="row">
@@ -67,121 +67,125 @@
           </div>
         </div>
 
-        <div class="col-md-6">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-header">
+              <h5>Control Directions</h5>
+            </div>
+            <div class="card-body" style="height: 260px">
+              <div class="buttons">
+                <button class="button up" value="up">
+                  <span class="arrow-up arrow"></span>
+                </button>
+                <button class="button down" value="down">
+                  <span class="arrow-down arrow"></span>
+                </button>
+                <button class="button right" value="right">
+                  <span class="arrow-right arrow"></span>
+                </button>
+                <button class="button left" value="left">
+                  <span class="arrow-left arrow"></span>
+                </button>
+              </div>
+              <button class="reset">Start</button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-4">
           <div class="card">
             <div class="card-header">
               <h5>Logs</h5>
             </div>
 
-            <div class="card-body" style="height: 250px; overflow-y: auto;">
-            <div v-for="(log, index) in logs" :key="index">
-            <small>{{ log }}</small>
-            </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-6 mt-4">
-          <div class="card">
-            <div class="card-header">
-              <h5>Control Directions</h5>
-            </div>
-            <div class="card-body" style="height: 260px;">
-              <div class="buttons">
-                <button class="button up"  value="up"><span class="arrow-up arrow"></span></button>
-                <button class="button down"  value="down"><span class="arrow-down arrow"></span></button>
-                <button class="button right"  value="right"><span class="arrow-right arrow"></span></button>
-                <button class="button left"  value="left"><span class="arrow-left arrow"></span></button>
+            <div class="card-body" style="height: 250px; overflow-y: auto">
+              <div v-for="(log, index) in logs" :key="index">
+                <small>{{ log }}</small>
               </div>
-              <button class="reset">Reset</button>
             </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
 </template>
 
 <style>
-  
-  .buttons{
-    position: relative;
-    width: 25rem;
-    height: 21rem;
-    /* top: 9rem; */
-    left: 4rem;
-  }
-  
-  .button{
-    position: absolute;
-    width: 3rem;
-    height: 3rem;
-  }
-  
-  .arrow{
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-  }
-  
-  .arrow-up{
-    border-left: 12px solid transparent;
-    border-right: 12px solid transparent;
-    border-bottom: 16px solid black;
-  }
-  
-  .arrow-right{
-    border-top: 12px solid transparent;
-    border-bottom: 12px solid transparent;
-    border-left: 16px solid black;
-  }
-  
-  .arrow-left{
-    border-top: 12px solid transparent;
-    border-bottom: 12px solid transparent;
-    border-right:16px solid black;
-  }
-  
-  .arrow-down{
-    border-left: 12px solid transparent;
-    border-right: 12px solid transparent;
-    border-top: 16px solid black;
-  }
-  
-  .down{
-    bottom: 11rem;
-    left: 9rem;
-  }
-  
-  .left{
-    top: 3.5rem;
-    left: 5rem;
-  }
-  
-  
-  .right{
-    top: 3.5rem;
-    right: 9rem;
-  }
-  
-  .up{
-    top: 0;
-    left: 9rem;
-  }
-  
-  .reset{
-    position: relative;
-    top: -10rem;
-    width: 7rem;
-    height: 3rem;
-    left: 11rem;
-  }
+.buttons {
+  position: relative;
+  width: 25rem;
+  height: 21rem;
+  /* top: 9rem; */
+  left: 4rem;
+}
+
+.button {
+  position: absolute;
+  width: 3rem;
+  height: 3rem;
+}
+
+.arrow {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.arrow-up {
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+  border-bottom: 16px solid black;
+}
+
+.arrow-right {
+  border-top: 12px solid transparent;
+  border-bottom: 12px solid transparent;
+  border-left: 16px solid black;
+}
+
+.arrow-left {
+  border-top: 12px solid transparent;
+  border-bottom: 12px solid transparent;
+  border-right: 16px solid black;
+}
+
+.arrow-down {
+  border-left: 12px solid transparent;
+  border-right: 12px solid transparent;
+  border-top: 16px solid black;
+}
+
+.down {
+  bottom: 11rem;
+  left: 9rem;
+}
+
+.left {
+  top: 3.5rem;
+  left: 5rem;
+}
+
+.right {
+  top: 3.5rem;
+  right: 9rem;
+}
+
+.up {
+  top: 0;
+  left: 9rem;
+}
+
+.reset {
+  position: relative;
+  top: -10rem;
+  width: 7rem;
+  height: 3rem;
+  left: 11rem;
+}
 </style>
 
 <script>
-
 import ROSLIB from "roslib";
 
 export default {
@@ -192,55 +196,52 @@ export default {
       isConnected: false,
       status: "Disconnected",
       ros: null,
-      logs: []
+      logs: [],
     };
   },
 
   methods: {
     connect() {
       this.ros = new ROSLIB.Ros({
-         url: this.url,
-       });
+        url: this.url,
+      });
 
       this.ros.on("connection", () => {
-         const message = "Connected to websocket server.";
-        this.writeLog(message)
-        console.log(message)
+        const message = "Connected to websocket server.";
+        this.writeLog(message);
+        console.log(message);
 
-         this.isConnected = true;
-         this.status = "Connected";
-       });
+        this.isConnected = true;
+        this.status = "Connected";
+      });
 
-       this.ros.on("error", (error) => {
-         this.writeLog(error)
-         console.log(error)
-       });
+      this.ros.on("error", () => {
+        const message = "Error connecting to websocket server.";
+        this.writeLog(message);
+        console.log(message);
+      });
 
-       this.ros.on("close", () => {
+      this.ros.on("close", () => {
         const message = "Disconnected from websocket server.";
-        this.writeLog(message)
-        console.log(message)
+        this.writeLog(message);
+        console.log(message);
 
-         this.isConnected = false;
-         this.status = "Disconnected";
-       });
-
-      // this.isConnected = true;
-      // this.status = "Connected";
+        this.isConnected = false;
+        this.status = "Disconnected";
+      });
     },
 
     disconnect() {
       this.ros.close();
-      // this.isConnected = false;
-      // this.status = "Disconnected";
     },
 
     writeLog(message) {
-      let date = new Date()
-      let logMessage = `[${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] ${message}`
-      this.logs.unshift(logMessage)
-
-    }
+      let date = new Date();
+      let logMessage = `[${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}] ${message}`;
+      this.logs.unshift(logMessage);
+    },
   },
 
   computed: {
