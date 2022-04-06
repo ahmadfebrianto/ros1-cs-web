e56443a01847e663006194b6cb2996a202dd5d89
mujibluth <frame.mujib789@gmail.com>
2022-04-06 16:04:26 +0700

    update navbar

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -2,14 +2,16 @@ app.component('navbar', {
   template:
     /*HTML*/
     `
-    <nav class="container-fluid navbar shadow gx-4 py-2">
-        <div class="row">
-            <div 
-                class="col-auto ml-auto rounded-pill" 
-                :class="this.$store.state.status === 'Connected' ? 'bg-success' : 'bg-danger'">
-                <small class="text-white">{{ this.$store.state.status }}</small>
-            </div>
-        </div>
+    <nav class="container-fluid navbar shadow gx-4 py-2 ml-auto">
+      <button type="button" id="sidebarCollapse" class="btn" style="color: white; background-color: #17a2b8; border-color: #17a2b8;">
+        <i class="fas fa-align-left"></i>
+        <span> Toggle Sidebar</span>
+      </button>
+      <div 
+        class="col-auto ml-auto rounded-pill" style="padding: 7px 20px;"
+        :class="this.$store.state.status === 'Connected' ? 'bg-success' : 'bg-danger'">
+        <small class="text-white">{{ this.$store.state.status }}</small>
+      </div>
     </nav>
     `,
 
