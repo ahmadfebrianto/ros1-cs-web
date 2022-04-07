49fa2bb0f163e3e02c4ad1fd0cff2ba7bab647f0
mujibluth <frame.mujib789@gmail.com>
2022-04-07 15:07:52 +0700

    update about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,14 @@ window.onbeforeunload = () => {
 const app = Vue.createApp({
   template: /*html*/ `
     
-    <div id="app">
-      <div class="row flex-nowrap">
-        <sidebar />
+    <div class="row flex-nowrap">
+      <sidebar />
 
-        <div id="content" class="col gx-0">
-          <navbar />
-          <main class="container">
-            <router-view />
-          </main>
-        </div>
+      <div id="content" class="col gx-0">
+        <navbar />
+        <main class="">
+          <router-view />
+        </main>
       </div>
     </div>
 
