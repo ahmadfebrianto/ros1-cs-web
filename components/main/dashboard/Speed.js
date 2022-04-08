06d34d7be09bb7de78e7ec8d5a4ed1f3963172e2
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-04-08 15:00:58 +0700

    Delete robot images

--- components/main/dashboard/Speed.js
+++ components/main/dashboard/Speed.js
@@ -38,3 +38,21 @@ app.component('speed', {
     this.speed = this.$store.state.robotSpeed * 10;
   },
 });
+
+
+{/* <div class="card shadow-sm mb-2">
+        <div class="card-body pt-2">
+            <div class="row">
+                <small class="text-muted">Speed</small>
+                <small class="text-danger">{{ speed }}</small>
+            </div>
+            <input 
+                  @change="setRobotSpeed" 
+                  v-model="speed"
+                  type="range" 
+                  class="form-range" 
+                  min="1" max="5" 
+                  step=1 
+                  id="customRange2">
+        </div>
+    </div> */}
