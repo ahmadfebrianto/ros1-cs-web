d8bd14d8cab2116f4a00cebb9988346adcd0f199
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-04-19 09:39:05 +0700

    Add start and stop button on interactive mode

--- components/main/dashboard/Interactive.js
+++ components/main/dashboard/Interactive.js
@@ -3,7 +3,14 @@ app.component('interactive', {
     `
     <div class="card shadow-sm">
         <div class="card-body pt-2">
-        <small class="text-muted">Mode</small>
+            <div class="row">
+                <div class="col-md-6 text-center">
+                    <a href="#" class="btn btn-primary">Start</a>
+                </div>
+                <div class="col-md-6 text-center">
+                    <a href="#" class="btn btn-danger">Stop</a>
+                </div>
+            </div>
         </div>
     </div>
     `,
