ea444d72d322722671ce9d67754ac02a68b2a822
Ahmad Febrianto <achmadfebryanto@gmail.com>
2022-05-25 12:21:56 +0700

    Change devPath in tauri config file

--- dist/components/main/dashboard/Interactive.js
+++ dist/components/main/dashboard/Interactive.js
@@ -2,11 +2,11 @@ app.component('interactive', {
   template: `
     <div class="card">
         <div class="flex flex-2 sm:flex-col md:flex-row gap-2 p-2">
-            <div class="button bg-red-500 hover:opacity-90 active:opacity-100"
+            <button class="button bg-red-500 hover:opacity-90 active:opacity-100"
                 @click="cancelGoal"
                 :class="{ 'pointer-events-none opacity-50': !connected }">
                 Cancel Goal
-            </div>
+            </button>
         </div>
     </div>
     `,
